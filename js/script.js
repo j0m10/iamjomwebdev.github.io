5d7f6eb47f91929e16bdb4dae98be2b6e8dce696
j0m10 <jomgallinera@gmail.com>
2025-01-26 17:45:23 +0800

    Update script.js
    new codes

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
-var name = window.prompt("Please Enter your Name: ");
-alert(" Welcome to our Website! " + name + "!")
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if(this.scrollY > 20){
+			$('.navbar').addClass("sticky");
+		}
+		else{
+			$('.navbar').removeClass("sticky")
+		}
+
+	});
+
+	$(".navbar .menu li a").click(function(){
+		$('html').css("scrollBehavior","smooth");
+
+	});
+
+	$('.menu-btn').click(function(){
+		$('.navbar .menu').toggleClass("active");
+		$('.menu-btn img').toggleClass("active");
+		
+	  });
+
+	});
